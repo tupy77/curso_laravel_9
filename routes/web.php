db6236d2f33328ce5bd48de03d9056e71805abfd
Franci <tupy77@hotmail.com>
2023-04-12 19:07:34 +0200

    Views

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,35 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
-    return ('Hola, por fin');
+    return view('home');
 });
 
 Route::get('/blog', function(){
-    return ('Hola, blog');
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel']
+    ];
+    return view('blog',['posts' => $posts]);
+    // return ('Hola, blog');
 });
 
 Route::get('/user', function(){
     return ('Hola, user');
 });
 
-Route::get('/user/{slug}', function($slug){
-    return $slug;
+Route::get('/blog/{slug}', function($slug){
+    $post = $slug;
+    return view('post',['post' => $post]);
+    // return $slug;
 });
 
-Route::get('buscar', function(Request $request){
-    return $request->all();
-});
+
+
+// Route::get('buscar', function(Request $request){
+//     return $request->all();
+// });
 /* /buscar?query=php */
+
