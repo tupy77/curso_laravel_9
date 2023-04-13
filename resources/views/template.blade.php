9a4b6badd284bccfff1b8255dc893a86329d869c
Franci <tupy77@hotmail.com>
2023-04-13 19:33:20 +0200

    Inicio de sesion, instalar breeze y node

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -10,6 +10,13 @@
     <p>
         <a href="{{ route('home') }}">Home</a>
         <a href="{{ route('blog') }}">Blog</a>
+
+		@auth
+		<a href="{{ route('dashboard') }}">Dashboard</a>
+		@else
+		<a href="{{ route('login') }}">Login</a>
+		@endif
+    
     </p>
 
     <hr>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 
 Route::controller(PageController::class)->group(function () {     
 
+    Route::get('/dashboard',           'home')->name('dashboard');  
     Route::get('/',           'home')->name('home');     
     Route::get('/blog',        'blog')->name('blog'); 
     Route::get('blog/{post:slug}', 'post')->name('post');
 
 });
 
-/** OTRA FORMA
-Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('blog', [PageController::class, 'blog'])->name('blog');
-Route::get('blog/{post:slug}', [PageController::class, 'post'])->name('post');
-*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+require __DIR__.'/auth.php';
